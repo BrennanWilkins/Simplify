51d00ba3e8cbb9fe2048ca84da2d0b96bf50598f
Brennan Wilkins <bwilkins767@gmail.com>
2020-07-09 18:33:51 -0500

    created build

---
+++ client/build/precache-manifest.e31c4f81511a15f65268ce97c90d3a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a999f267c4f309ca8dd50c1c9f696b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2918b4e99afaddf249ee",
+    "url": "/static/css/main.97299d3f.chunk.css"
+  },
+  {
+    "revision": "fc2576edafe045c1f293",
+    "url": "/static/js/2.2d6de35d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2d6de35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2918b4e99afaddf249ee",
+    "url": "/static/js/main.bf4ec564.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
