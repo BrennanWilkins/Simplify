99aadd96254ccd75311fd65f2dd9edf765885f44
Brennan Wilkins <bwilkins767@gmail.com>
2020-07-09 18:30:58 -0500

    deployment

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const authInstance = axios.create({
-  baseURL: 'http://localhost:9000/api/auth/'
+  // baseURL: 'http://localhost:9000/api/auth/'
+  baseURL: 'https://simplify.herokuapp.com/api/auth/'
 });
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:9000/api/'
+  // baseURL: 'http://localhost:9000/api/'
+  baseURL: 'https://simplify.herokuapp.com/api/'
 });
