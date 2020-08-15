580424c0e6f736c1fbff70a8942caf87c1e554ce
Brennan Wilkins <bwilkins767@gmail.com>
2020-08-15 12:52:55 -0500

    Fixed panel css

--- client/src/components/NewGoalPanel/NewGoalPanel.js
+++ client/src/components/NewGoalPanel/NewGoalPanel.js
@@ -63,46 +63,49 @@ class NewGoalPanel extends React.Component {
 
   render() {
     return (
-      <div className={this.props.show ? classes.Panel : classes.HidePanel} ref={this.panel}>
-        <CloseBtn close={this.closeHandler} />
-        <div className={classes.Content}>
-          <div className={this.state.currPage === 1 ? classes.Show : classes.HideLeft}>
-            <h3>What is the name of your goal?</h3>
-            <Input val={this.state.goalName} change={val => this.setState({ goalName: val })} />
-            <p className={classes.SubTitle}>Here are some ideas to inspire you</p>
-            <div className={classes.Examples}>
-              <p>Pay off credit card debt</p>
-              <p>Create an emergency fund</p>
-              <p>Buy a home</p>
-              <p>Save for college</p>
-              <p>Save for retirement</p>
-              <p>Buy a new car</p>
-              <p>Save for a vacation trip</p>
+      <React.Fragment>
+        <div className={this.props.show ? classes.Panel : classes.HidePanel} ref={this.panel}>
+          <CloseBtn close={this.closeHandler} />
+          <div className={classes.Content}>
+            <div className={this.state.currPage === 1 ? classes.Show : classes.HideLeft}>
+              <h3>What is the name of your goal?</h3>
+              <Input val={this.state.goalName} change={val => this.setState({ goalName: val })} />
+              <p className={classes.SubTitle}>Here are some ideas to inspire you</p>
+              <div className={classes.Examples}>
+                <p>Pay off credit card debt</p>
+                <p>Create an emergency fund</p>
+                <p>Buy a home</p>
+                <p>Save for college</p>
+                <p>Save for retirement</p>
+                <p>Buy a new car</p>
+                <p>Save for a vacation trip</p>
+              </div>
+            </div>
+            <div className={this.state.currPage === 2 ? classes.Show : (this.state.hides.includes(2) ? classes.HideLeft : classes.HideRight)}>
+              <h3>What amount do you want to save?</h3>
+              <NumInput val={this.state.goalVal} change={val => this.setState({ goalVal: val })} />
+            </div>
+            <div className={this.state.currPage === 3 ? classes.Show : (this.state.hides.includes(3) ? classes.HideLeft : classes.HideRight)}>
+              <h3>When do you want to reach your goal by? (optional)</h3>
+              <DateInput val={this.state.goalDate} change={val => this.setState({ goalDate: val })} />
+            </div>
+            <div className={this.state.currPage === 4 ? classes.Show : (this.state.hides.includes(4) ? classes.HideLeft : classes.HideRight)}>
+              <p className={classes.P1}>It can help to keep the money for your goal in a separate place, like a different bank account.</p>
+              <p>Whenever you add money to this account, you can add a contribution on the goals page.</p>
+              <p className={this.state.err ? classes.ShowErrMsg : classes.HideErrMsg}>{this.state.errMsg}</p>
             </div>
           </div>
-          <div className={this.state.currPage === 2 ? classes.Show : (this.state.hides.includes(2) ? classes.HideLeft : classes.HideRight)}>
-            <h3>What amount do you want to save?</h3>
-            <NumInput val={this.state.goalVal} change={val => this.setState({ goalVal: val })} />
-          </div>
-          <div className={this.state.currPage === 3 ? classes.Show : (this.state.hides.includes(3) ? classes.HideLeft : classes.HideRight)}>
-            <h3>When do you want to reach your goal by? (optional)</h3>
-            <DateInput val={this.state.goalDate} change={val => this.setState({ goalDate: val })} />
-          </div>
-          <div className={this.state.currPage === 4 ? classes.Show : (this.state.hides.includes(4) ? classes.HideLeft : classes.HideRight)}>
-            <p className={classes.P1}>It can help to keep the money for your goal in a separate place, like a different bank account.</p>
-            <p>Whenever you add money to this account, you can add a contribution on the goals page.</p>
-            <p className={this.state.err ? classes.ShowErrMsg : classes.HideErrMsg}>{this.state.errMsg}</p>
+          <div className={classes.BtnsDiv}>
+            <div className={this.state.currPage === 1 ? classes.DisPrevBtn : classes.PrevBtn} onClick={this.prevHandler}>
+              <span className={classes.Icon}>{arrowRight}</span>
+            </div>
+            <div className={this.state.currPage === 4 ? classes.DisNextBtn : classes.NextBtn} onClick={this.nextHandler}>
+              <span className={classes.Icon}>{arrowRight}</span>
+            </div>
           </div>
         </div>
-        <div className={classes.BtnsDiv}>
-          <div className={this.state.currPage === 1 ? classes.DisPrevBtn : classes.PrevBtn} onClick={this.prevHandler}>
-            <span className={classes.Icon}>{arrowRight}</span>
-          </div>
-          <div className={this.state.currPage === 4 ? classes.DisNextBtn : classes.NextBtn} onClick={this.nextHandler}>
-            <span className={classes.Icon}>{arrowRight}</span>
-          </div>
-        </div>
-      </div>
+        <div className={this.props.show ? classes.Backdrop : classes.HideBackdrop}></div>
+      </React.Fragment>
     );
   }
 }
