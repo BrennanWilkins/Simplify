e81f81d540d620bfa8fa75ecf33cd7df1e67f038
Brennan Wilkins <bwilkins767@gmail.com>
2020-08-25 13:14:08 -0500

    Updated css

--- client/src/components/PortfolioPage/NetWorthChart/NetWorthChart.js
+++ client/src/components/PortfolioPage/NetWorthChart/NetWorthChart.js
@@ -2,13 +2,31 @@ import React from 'react';
 import classes from './NetWorthChart.module.css';
 import { connect } from 'react-redux';
 import CanvasJSReact from '../../UI/canvasjs/canvasjs.react';
-import { createNetWorthOptions } from '../../../utils/chartFuncs';
 import { formatNum } from '../../../utils/formatNum';
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const NetWorthChart = props => {
   const netWorthVal = props.netWorthData.length === 0 ? '0.00' :
   formatNum(props.netWorthData[props.netWorthData.length - 1].value);
+
+  const netWorthData = props.netWorthData.map(dataPt => ({ x: new Date(dataPt.date), y: Number((dataPt.value).toFixed(2)) }));
+
+  const options = {
+    animationEnabled: true,
+    theme: 'light2',
+    exportEnabled: false,
+    axisY: { valueFormatString: "'$'0" },
+    axisX: { valueFormatString: 'MM/DD/YY', labelAngle: -20 },
+    toolTip: { content: '{x}: ${y}' },
+    data: [{
+      type: 'area',
+      indexLabelFontColor: '#5A5757',
+      indexLabelPlacement: 'outside',
+      color: 'rgb(26, 171, 152)',
+      fillOpacity: '1',
+      dataPoints: netWorthData
+    }]
+  };
 
   // props.small used for chart on home page
   return (
@@ -19,7 +37,7 @@ const NetWorthChart = props => {
           <h1 className={classes.NetWorthValueSmall}>${netWorthVal}</h1>
         </div>
         <div className={classes.NetWorthChartSmall}>
-          <CanvasJSChart options={{ ...createNetWorthOptions(props.netWorthData), height: 200 }} />
+          <CanvasJSChart options={{ ...options, height: 200 }} />
           <div className={classes.Block}></div>
         </div>
       </div>
@@ -30,7 +48,7 @@ const NetWorthChart = props => {
           <h1 className={classes.NetWorthValue}>${netWorthVal}</h1>
         </div>
         <div className={classes.NetWorthChart}>
-          <CanvasJSChart options={{ ...createNetWorthOptions(props.netWorthData) }} />
+          <CanvasJSChart options={options} />
           <div className={classes.Block}></div>
         </div>
       </div>
